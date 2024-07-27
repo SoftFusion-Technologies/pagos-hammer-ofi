c4cb164d8296aa55b1b1f319017a27ae953e5233
Benjamin <goosta19802@gmail.com>
2024-07-27 15:15:54 -0300

    Mejoras promociones

--- src/Components/Promociones.jsx
+++ src/Components/Promociones.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
-import Trimestral from '../Images/trimestral.png'; // Actualiza la ruta a la imagen que subiste
-import Semestral from '../Images/semestral.png'; // Actualiza la ruta a la imagen que subiste
-import Anual from '../Images/anual.png'; // Actualiza la ruta a la imagen que subiste
-import Macro from '../Images/macro.png'; // Actualiza la ruta a la imagen que subiste
+import Trimestral from '../Images/trimestral.png';
+import Semestral from '../Images/semestral.png';
+import Anual from '../Images/anual.png';
+import Macro from '../Images/macro.png';
+
+const Plan = ({ imageSrc, altText, message }) => {
+  const encodedMessage = encodeURIComponent(
+    `Hola! Vengo desde la pagina web, quiero mas info sobre "${message}" :)`
+  );
+  const whatsappUrl = `https://api.whatsapp.com/send?phone=543865855100&text=${encodedMessage}`;
+
+  return (
+    <div className="square-div">
+      <a href={whatsappUrl} target="_blank" rel="noopener noreferrer">
+        <img src={imageSrc} alt={altText} />
+      </a>
+    </div>
+  );
+};
 
 const Promociones = () => {
   return (
@@ -10,18 +25,26 @@ const Promociones = () => {
       <h1 className="message font-bignoodle m2">
         MIRÁ NUESTRAS PROMOCIONES -----------------------
       </h1>
-      <div className="square-div">
-        <img src={Trimestral} alt="Promoción Trimestral" />
-      </div>
-      <div className="square-div">
-        <img src={Semestral} alt="Promoción Semestral" />
-      </div>
-      <div className="square-div">
-        <img src={Anual} alt="Promoción Anual" />
-      </div>
-      <div className="square-div">
-        <img src={Macro} alt="Promoción Macro" />
-      </div>
+      <Plan
+        imageSrc={Trimestral}
+        altText="Promoción Trimestral"
+        message="PLAN TRIMESTRAL MONTEROS"
+      />
+      <Plan
+        imageSrc={Semestral}
+        altText="Promoción Semestral"
+        message="PLAN SEMESTRAL MONTEROS"
+      />
+      <Plan
+        imageSrc={Anual}
+        altText="Promoción Anual"
+        message="PLAN ANUAL MONTEROS"
+      />
+      <Plan
+        imageSrc={Macro}
+        altText="Promoción Macro"
+        message="PLAN MACRO MONTEROS"
+      />
     </div>
   );
 };
